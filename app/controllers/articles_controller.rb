db4d74fc82f6126aecc04ac34792bb5fcc3ded12
Curtis Atmarow <catmarow@gmail.com>
2022-03-31 00:03:00 -0500

    Added Create Read/Show Edit/Update actions to the Alpha Blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,4 +9,39 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    #byebug
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    #render plain: params[:article]
+    @article = Article.new(params.require(:article).permit(:title, :description))
+
+    #render plain: @article.inspect
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      #redirtect to the "show" page to show the results of this "create" to the creating user. Either of the below lines will work.
+      #redirect_to article_path(@article)
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    #byebug
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+        flash[:notice] = "Article was updated successfully."
+        redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
 end
