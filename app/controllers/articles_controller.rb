e53d323f9aa04574315864c6f84b9a22118a6ae5
Curtis Atmarow <catmarow@gmail.com>
2022-04-13 23:32:59 -0500

    Added Layout Links and did some DRY Code cleanup

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,8 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def show
     #byebug
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -15,12 +15,11 @@ class ArticlesController < ApplicationController
 
   def edit
     #byebug
-    @article = Article.find(params[:id])
   end
 
   def create
     #render plain: params[:article]
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
 
     #render plain: @article.inspect
     if @article.save
@@ -35,13 +34,27 @@ class ArticlesController < ApplicationController
 
   def update
     #byebug
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
         flash[:notice] = "Article was updated successfully."
         redirect_to @article
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  #resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy] --use when need to expose certain actions, but not all of them.
+  resources :articles
 end
